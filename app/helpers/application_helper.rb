a0990d512ccd92864a1f12433dfe2739d61a69f0
Laura Gomez <laura0703@gmail.com>
2014-10-07 17:23:52 -0500

    Improve flash messages styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,13 @@ module ApplicationHelper
     end
     html.html_safe
   end
+
+  def flash_class(level)
+    case level
+      when :notice  then "alert alert-info"
+      when :success then "alert alert-success"
+      when :error   then "alert alert-danger"
+      when :alert   then "alert alert-warning"
+    end
+  end
 end
