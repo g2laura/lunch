75bfe1766eebcfbc526bc9fd0e59051427aaa39d
Laura Gomez <laura0703@gmail.com>
2014-10-14 23:23:46 -0500

    Add helper to show an image background f

--- app/helpers/restaurants_helper.rb
+++ app/helpers/restaurants_helper.rb
@@ -1,2 +1,9 @@
 module RestaurantsHelper
+  def image_url(restaurant)
+    unless restaurant.image.url.blank?
+      restaurant.image.url(:thumb).to_s
+    else
+      "http://placehold.it/250x200&text=" + restaurant.name
+    end
+  end
 end
