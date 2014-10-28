2f4290ae6c2394131a115b1b78951ec625d223d0
Laura Gomez <laura0703@gmail.com>
2014-10-28 16:36:25 -0500

    Fix bug with restaurants ordered_by_votes

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -44,9 +44,7 @@ class RestaurantsController < ApplicationController
 
   def vote
     @restaurant = Restaurant.find(params[:restaurant_id])
-    unless current_user.restaurant == @restaurant
-      current_user.update(restaurant: @restaurant)
-    end
+    current_user.update(restaurant: @restaurant)
   end
 
   private
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -8,6 +8,6 @@ class Restaurant < ActiveRecord::Base
   validates :name, :phone_number, presence: true
 
   scope :ordered_by_votes, -> {
-    find_by_sql("SELECT restaurants.*, COUNT(users.id) AS votes FROM users, restaurants WHERE users.restaurant_id = restaurants.id OR users.restaurant_id IS NULL GROUP BY restaurants.id ORDER BY votes DESC")
+    find_by_sql("SELECT restaurants.*, COUNT(users.restaurant_id) AS votes FROM restaurants LEFT JOIN users ON users.restaurant_id = restaurants.id GROUP BY restaurants.id ORDER BY votes DESC")
   }
 end
