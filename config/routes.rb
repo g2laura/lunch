fd56fbb607116de6315ae156d21f6c25d333f58a
meuray <raymar.vernaza@admios-sa.com>
2015-02-06 01:48:27 -0500

    Generate a new route

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Lunch::Application.routes.draw do
 
   get 'order/restaurant_orders/:restaurant_id', as: 'lunch', to: 'order#restaurant_orders', defaults: { format: :pdf }
 
+  get '/restaurants/:restaurant_id/items', to: 'items#watchmenu', as: 'restmenu'
+
   resources :restaurants do
     get :vote, defaults: { format: :json }
     resources :items
